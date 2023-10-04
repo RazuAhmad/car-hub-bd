7f32648f03fe2105ca3e4430f9d51af172e4615a
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-10-04 10:45:43 +0600

    done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return <div className="max-width padding-x">Footer</div>;
+};
 
-export default Footer
+export default Footer;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,35 @@
-import Button from '@mui/material/Button'
-import { blue } from '@mui/material/colors'
-import Image from 'next/image'
-import React from 'react'
+import Button from "@mui/material/Button";
+import { blue } from "@mui/material/colors";
+import Image from "next/image";
+import React from "react";
 
-import heroCarImg from '../public/assets/hero.png'
+import heroCarImg from "../public/assets/hero.png";
 
 const Hero = () => {
-    return (
-        <div className='hero sm:px-16 px-6'>
+  return (
+    <div className="hero items-center sm:px-16 px-6">
+      <div className="pt-36 flex-1">
+        <h1 className="hero__title">
+          Find, book, or rent a car --quickly and easily!
+        </h1>
 
-            <div className='pt-36 flex-1'>
-                <h1 className='hero__title'>Find, book, or rent a car --quickly and easily!</h1>
+        <p className="hero__subtitle mb-4">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <Button
+          variant="contained"
+          sx={{ color: "black", backgroundColor: blue }}
+        >
+          Explore Cars
+        </Button>
+      </div>
 
-                <p className="hero__subtitle">
-                    Streamline your car rental experience with our effortless booking process.
-                </p>
-                <Button variant='contained' sx={{ color: 'black', backgroundColor: blue }}>Explore Cars</Button>
-            </div>
+      <div className="hero__image flex-[1.5]">
+        <Image src={heroCarImg} alt="car hero" className="object-contain" />
+      </div>
+    </div>
+  );
+};
 
-            <div className='hero__image-container '>
-                <div className="hero__image">
-                    <Image src={heroCarImg} alt='car hero' className='object-contain' />
-
-                </div>
-            </div>
-
-        </div>
-    )
-}
-
-export default Hero 
+export default Hero;
